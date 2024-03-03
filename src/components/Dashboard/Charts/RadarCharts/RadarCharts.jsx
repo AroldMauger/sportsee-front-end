32f52f3c6f07494faa7e49b6e12af4019045a5f8
Arold Mauger <aroldmauger2022@gmail.com>
2024-03-03 22:30:53 +0100

    Ajout de l'espacement entre les légendes et le graphique en modifiant manuellement les valeurs dx et dy

--- src/components/Dashboard/Charts/RadarCharts/RadarCharts.jsx
+++ src/components/Dashboard/Charts/RadarCharts/RadarCharts.jsx
@@ -34,13 +34,63 @@ function RadarCharts() {
     kind: translationsToFrench[kind],
     value: data.find(item => kinds[item.kind] === kind).value // on cherche dans "data" l'éléments dont le kind correspond au kind dans map(kind =>)
   }));
-
+  function renderOuterTick(props) {
+    const { x, y, payload } = props;
+    let dx = 0;
+    let dy = 0;
+  
+    // On ajuste la position de chaque légende sur l'axe dx et dy en fonction de la maquette
+    switch (payload.index) {
+      case 0:
+        dy = -30;
+        break;
+      case 1:
+        dx = 40;
+        dy = -10;
+        break;
+      case 2:
+        dx = 40;
+        dy = 15;
+        break;
+      case 3:
+        dy = 30;
+        break;
+      case 4:
+        dx = -40;
+        dy = 15;
+        break;
+      case 5:
+        dx = -40;
+        dy = -10;        
+        break;
+      case 6:
+        dy = 25;
+      default:
+        break;
+    }
+  
+    return (
+      <text 
+        x={x + dx} 
+        y={y + dy} 
+        fill="#FFFFFF" 
+        fontSize={12} 
+        textAnchor="middle"
+        dominantBaseline="middle"
+      >
+        {payload.value}
+      </text>
+    );
+  }
+  
+  
   return (
     <ResponsiveContainer width={300} aspect={1} style={{ width: "100%", height: "100%", backgroundColor: '#282D30', borderRadius: "10px" }} >
-      <RadarChart cx="50%" cy="50%" outerRadius="50%" data={dataInChart} >
-        <PolarGrid  radialLines={false} polarRadius={[0, 10, 20, 40, 72, 95]} />
+      <RadarChart cx="50%" cy="50%" outerRadius="50%" data={dataInChart}  >
+        <PolarGrid  radialLines={false} polarRadius={[0, 10, 20, 40, 72, 95]} strokeWidth={1.2} />
         <PolarAngleAxis dataKey="kind"  stroke="#FFFFFF" tickLine={false} 
-        tick={{ fill: "white", fontSize: 12 }}
+  tick={renderOuterTick}
+
          />
         <PolarRadiusAxis fill="#FFFFFF"  stroke="none" />
         
